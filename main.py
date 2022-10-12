4046303e63e738b0a16e6023e3cf69ddbaf765e2
andrew.bunn <andrew.bunn@wsu.edu>
2022-10-11 18:25:48 -0700

    Initial Checkin

---
+++ main.py
@@ -0,0 +1,20 @@
+
+class AVLNode:
+    def __init__(self, user, serverBannedOn, timeOfBan):
+        self.user = user
+        self.serverBannedOn = serverBannedOn
+        self.timeOfBan = timeOfBan
+        self.left = None
+        self.right = None
+        self.balance = 1
+
+
+# class AVLTree:
+
+
+if __name__ == '__main__':
+    # read input from file and insert into a tree ORG ON USER NAME
+
+    # read names from stdin
+    print("Hi")
+
