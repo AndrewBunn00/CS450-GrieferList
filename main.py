e07d88c62f17b9219f50cc7ddee5a892fb98a153
andrew.bunn <andrew.bunn@wsu.edu>
2022-10-14 16:32:06 -0700

    Started working on scapegoat tree
    Begin working on finding scapegoat

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import sys
+import math
+
+
+class ScapeGoatNode:
+    def __init__(self, user, serverBannedOn, timeOfBan):
+        self.user = user
+        self.serverBannedOn = serverBannedOn
+        self.timeOfBan = timeOfBan
+        self.left = None
+        self.right = None
+        self.parent = None
 
 
 class AVLNode:
@@ -12,7 +22,88 @@ class AVLNode:
         self.balance = 0
 
 
+class ScapeGoatTree:
+    def __init__(self, alpha):
+        self.root = None
+        self.size = 0
+        self.alpha = alpha
+
+    def insert(self, user, serverBannedOn, timeOfBan):
+        newNode = ScapeGoatNode(user, serverBannedOn, timeOfBan)
+        depth = 0
+        # if tree empty, the new node will be the root
+        if(self.root == None):
+            # root = ScapeGoatNode(user, serverBannedOn, timeOfBan)
+            self.size += 1
+            self.root = newNode
+            return
+
+        curRoot = self.root
+        prevRoot = None
+        parentList = []
+
+        while curRoot is not None:
+            prevRoot = curRoot
+            parentList.append(prevRoot)
+            # perform bst insert, but track depth and list of parents as we go
+            if(user < curRoot.user):
+                curRoot = curRoot.left
+                depth += 1
+            elif(user >= curRoot.user):
+                curRoot = curRoot.right
+                depth += 1
+
+        newNode.parent = prevRoot
+
+        # Now insert the new node
+        if(newNode.user < newNode.parent.user):
+            newNode.parent.left = newNode
+        elif(newNode.user >= newNode.parent.user):
+            newNode.parent.right = newNode
+
+        self.size += 1
+
+        # if depth > alphaHeight(tree of size T) find scapegoat and rebalance
+        if(depth > self.alphaHeight()):
+            # scapeGoat = self.findScapeGoat()
+            print("Left off here")
+        # To find goat, go backward up list of parents until we find highest imbalanced parent
+
+        # Do in-order traversal of tree starting at the highest imbalanced parent to get arr of sorted vals
+
+        # use values to rebuild tree
+
+
+    def sizeOfTree(self, node):
+        if node is None:
+            return 0
+        else:
+            return self.sizeOfTree(node.left) + self.sizeOfTree(node.right) + 1
+
+
+
+    def alphaHeight(self):
+        """
+        :return: the alpha height of the tree
+        """
+        return math.floor(math.log(self.size, 1/self.alpha))
+
+
+    def isAlphaBalanced(self, node):
+        """
+        check if the tree is alpha balanced by checking the balances of the left and right subtrees
+        :param node:
+        :return: returns true if balanced, else false
+        """
+        isLeftBal = self.sizeOfTree(node.left) <= self.alpha * self.sizeOfTree(node)
+        isRightBal = self.sizeOfTree(node.right) <= self.alpha * self.sizeOfTree(node)
+        return isLeftBal and isRightBal
+
+
 class AVLTree:
+    # def __init__(self):
+    #     self.root = None
+
 
     def insert(self, root, user, serverBannedOn, timeOfBan):
         # user is key
@@ -109,8 +200,14 @@ class AVLTree:
 
 
 if __name__ == '__main__':
-    tree = AVLTree()
-    root = None
+    # SCAPEGOAT AND TEST CASES =========================================
+
+    # =========================================
+
+
+    # AVL AND TEST CASES =========================================
+    # tree = AVLTree()
+    # root = None
 
     # for line in sys.stdin:
     #     line = line.split()
@@ -141,6 +238,8 @@ if __name__ == '__main__':
     # root = tree.insert(root, 30, 0, 0)
     # root = tree.insert(root, 40, 0, 0)
     # root = tree.insert(root, 50, 0, 0)
+    # ==================================================
+
 
     # read input from file and insert into a tree ORG ON USER NAME
 
