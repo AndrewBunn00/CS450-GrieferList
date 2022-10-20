e90028857a6c64a4d5348d2cbecab2a9c41d4080
andrew.bunn <andrew.bunn@wsu.edu>
2022-10-19 18:44:48 -0700

    Scapegoat works

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import sys
+import time
 
 
 class ScapeGoatNode:
@@ -25,6 +26,7 @@ class AVLNode:
 
 class ScapeGoatTree:
     def __init__(self, alpha):
+        self.results = []
         self.root = None
         self.size = 0
         self.alpha = alpha
@@ -33,7 +35,7 @@ class ScapeGoatTree:
         newNode = ScapeGoatNode(user, serverBannedOn, timeOfBan)
         depth = 0
         # if tree empty, the new node will be the root
-        if(self.root == None):
+        if (self.root == None):
             # root = ScapeGoatNode(user, serverBannedOn, timeOfBan)
             self.size += 1
             self.root = newNode
@@ -47,38 +49,45 @@ class ScapeGoatTree:
             prevRoot = curRoot
             parentList.append(prevRoot)
             # perform bst insert, but track depth and list of parents as we go
-            if(user < curRoot.user):
+            if (user < curRoot.user):
                 curRoot = curRoot.left
                 depth += 1
-            elif(user >= curRoot.user):
+            elif (user >= curRoot.user):
                 curRoot = curRoot.right
                 depth += 1
 
         newNode.parent = prevRoot
 
         # Now insert the new node
-        if(newNode.user < newNode.parent.user):
+        if (newNode.user < newNode.parent.user):
             newNode.parent.left = newNode
-        elif(newNode.user >= newNode.parent.user):
+        elif (newNode.user >= newNode.parent.user):
             newNode.parent.right = newNode
 
         self.size += 1
 
         # if depth > alphaHeight(tree of size T) find scapegoat and rebalance
-        if(depth > self.alphaHeight()):
-            # scapeGoat = self.findScapeGoat()
-            print("Left off here")
-        # To find goat, go backward up list of parents until we find highest imbalanced parent
-            highestParent = None
+        if (depth > self.alphaHeight()):
+            # To find goat, go backward up list of parents until we find highest imbalanced parent
+            # set lowest parent as starting point
+            highestParent = parentList[-1]
             for i, parent in enumerate(parentList[::-1]):
-                if(i > self.alphaHeight(parent)):
+
+                if (i > self.alphaHeight(parent)):
                     highestParent = parent
-                    break
 
-
-        # Do in-order traversal of tree starting at the highest imbalanced parent to get arr of sorted vals
-
-        # use values to rebuild tree
+            # Do in-order traversal of tree starting at the highest imbalanced parent to get arr of sorted vals
+            subRoot = highestParent
+            inOrderNodes = []
+            inOrderNodes = self.inOrderTraversal(subRoot, inOrderNodes)
+            # TODO figure out why parent is not assigned on certain case... depth 13
+            # use inOrderNodes to rebuild tree
+            newSubRoot = self.binarySearch(inOrderNodes, 0, len(inOrderNodes) - 1)
+            if(highestParent != self.root):
+                newSubRoot.parent = highestParent.parent
+                highestParent = newSubRoot
+            elif(highestParent == self.root):
+                self.root = newSubRoot
 
 
     def sizeOfTree(self, node):
@@ -88,16 +97,14 @@ class ScapeGoatTree:
             return self.sizeOfTree(node.left) + self.sizeOfTree(node.right) + 1
 
 
-
     def alphaHeight(self, specifiedRoot=None):
         """
         :return: the alpha height of the tree
         """
-        if(specifiedRoot is None):
-            return math.floor(math.log(self.size, 1/self.alpha))
+        if (specifiedRoot is None):
+            return math.floor(math.log(self.size, 1 / self.alpha))
         else:
-            return math.floor(math.log(self.sizeOfTree(specifiedRoot), 1/self.alpha))
-
+            return math.floor(math.log(self.sizeOfTree(specifiedRoot), 1 / self.alpha))
 
     def isAlphaBalanced(self, node):
         """
@@ -109,36 +116,75 @@ class ScapeGoatTree:
         isRightBal = self.sizeOfTree(node.right) <= self.alpha * self.sizeOfTree(node)
         return isLeftBal and isRightBal
 
+    def inOrderTraversal(self, subRoot, inOrderNodes):
+        if (subRoot):
+            self.inOrderTraversal(subRoot.left, inOrderNodes)
+            inOrderNodes.append(subRoot)
+            self.inOrderTraversal(subRoot.right, inOrderNodes)
+        return inOrderNodes
+
+    def binarySearch(self, inOrderNodes, low, high):
+        if low > high:
+            return
+
+        mid = (low + high) // 2
+        newRoot = ScapeGoatNode(inOrderNodes[mid].user, inOrderNodes[mid].serverBannedOn, inOrderNodes[mid].timeOfBan)
+        newRoot.left = self.binarySearch(inOrderNodes, low, mid - 1)
+        newRoot.right = self.binarySearch(inOrderNodes, mid + 1, high)
+        return newRoot
+
+
+    def getPlayer(self, root, wantedUser):
+        # walk the tree and add the found player to the results list
+        if (root == None):
+            return
+
+        if (root.user == wantedUser):
+            self.results.append(root)
+
+        self.getPlayer(root.left, wantedUser)
+        self.getPlayer(root.right, wantedUser)
+
+
+    def isPlayerBanned(self, wantedUser):
+        self.getPlayer(tree.root, wantedUser)
+        if (len(tree.results) == 0):
+            print(f"{wantedUser} is not currently banned from any servers")
+        else:
+            mostRecentTime = max(node.timeOfBan for node in self.results)
+            print(f"{wantedUser} was banned {len(tree.results)} times. "
+                  f"Most recently on {mostRecentTime}")
+            self.results = []
+
 
 class AVLTree:
     def __init__(self):
         self.results = []
 
-
     def insert(self, root, user, serverBannedOn, timeOfBan):
         # user is key
-        if(user < root.user):
-            if(root.left):
+        if (user < root.user):
+            if (root.left):
                 beforeBalance = root.left.balance
                 root.left = self.insert(root.left, user, serverBannedOn, timeOfBan)
                 afterBalance = root.left.balance
 
                 # left subtree changed height
-                if(beforeBalance == 0 and afterBalance != 0):
+                if (beforeBalance == 0 and afterBalance != 0):
                     root.balance -= 1
-                elif(beforeBalance != 0 and afterBalance == 0):
+                elif (beforeBalance != 0 and afterBalance == 0):
                     root.balance += 1
             else:
                 root.left = AVLNode(user, serverBannedOn, timeOfBan)
                 root.balance -= 1
 
-        if(root.balance < -1 and root.left.balance <= -1):
+        if (root.balance < -1 and root.left.balance <= -1):
             return self.rotRight(root)
-        if(root.balance < -1 and root.left.balance >= 1):
+        if (root.balance < -1 and root.left.balance >= 1):
             return self.rotLeftRight(root)
 
-        if(user >= root.user):
-            if(root.right):
+        if (user >= root.user):
+            if (root.right):
                 beforeBalance = root.right.balance
                 root.right = self.insert(root.right, user, serverBannedOn, timeOfBan)
                 afterBalance = root.right.balance
@@ -158,19 +204,17 @@ class AVLTree:
         if (root.balance > 1 and root.right.balance <= -1):
             return self.rotRightLeft(root)
 
-
         return root
-
 
     def rotLeft(self, root):
         newRoot = root.right
 
         root.right = root.right.left
         newRoot.left = root
-        if(newRoot.balance == 0 and (root.left or root.right)):
+        if (newRoot.balance == 0 and (root.left or root.right)):
             newRoot.balance = -1
             root.balance = 1
-        elif(newRoot.balance == 0):
+        elif (newRoot.balance == 0):
             newRoot.balance = -1
             root.balance = 0
         else:
@@ -178,17 +222,16 @@ class AVLTree:
             root.balance = 0
 
         return newRoot
-
 
     def rotRight(self, root):
         newRoot = root.left
 
         root.left = root.left.right
         newRoot.right = root
-        if(newRoot.balance == 0 and (root.left or root.right)):
+        if (newRoot.balance == 0 and (root.left or root.right)):
             newRoot.balance = 1
             root.balance = 1
-        elif(newRoot.balance == 0):
+        elif (newRoot.balance == 0):
             newRoot.balance = 1
             root.balance = 0
         else:
@@ -196,7 +239,6 @@ class AVLTree:
             root.balance = 0
 
         return newRoot
-
 
     def rotLeftRight(self, curNode):
         curNode.left = self.rotLeft(curNode.left)
@@ -206,13 +248,12 @@ class AVLTree:
         curNode.right = self.rotRight(curNode.right)
         return self.rotLeft(curNode)
 
-
     def getPlayer(self, root, wantedUser):
         # walk the tree and add the found player to the results list
-        if(root == None):
+        if (root == None):
             return
 
-        if(root.user == wantedUser):
+        if (root.user == wantedUser):
             self.results.append(root)
 
         self.getPlayer(root.left, wantedUser)
@@ -222,7 +263,7 @@ class AVLTree:
 
     def isPlayerBanned(self, root, wantedUser):
         self.getPlayer(root, wantedUser)
-        if(len(tree.results) == 0):
+        if (len(tree.results) == 0):
             print(f"{wantedUser} is not currently banned from any servers")
         else:
             mostRecentTime = max(node.timeOfBan for node in self.results)
@@ -231,27 +272,28 @@ class AVLTree:
             self.results = []
 
 
+
 if __name__ == '__main__':
     # SCAPEGOAT AND TEST CASES =========================================
 
     # =========================================
 
-
     # AVL AND TEST CASES =========================================
-    tree = AVLTree()
-    root = None
+    start_time = time.time_ns()
 
     with open(sys.argv[2]) as file:
-        if sys.argv[1] == "avl":
+        if (sys.argv[1] == "avl"):
+
+            tree = AVLTree()
+            root = None
+
             for line in file.readlines():
                 line = line.split()
-                # print(f"The line is {line}")
-                if(root == None):
+                if (root == None):
                     # first thing read should be root
                     root = AVLNode(line[0], line[1], line[2])
                 else:
                     root = tree.insert(root, line[0], line[1], line[2])
-            print("Tree built ==============")
 
             with open(sys.argv[3]) as inputFile:
                 for line in inputFile.readlines():
@@ -259,41 +301,27 @@ if __name__ == '__main__':
                     resList = []
                     tree.isPlayerBanned(root, line[0])
 
-            print("Done checking bans ======================")
 
+        elif(sys.argv[1] == "scapegoat"):
 
+            tree = ScapeGoatTree(0.9)
+            root = None
 
-    # if(root == None):
-    #     root = AVLNode(10, 0, 0)
+            for line in file.readlines():
+                line = line.split()
+                tree.insert(line[0], line[1], line[2])
 
-    # Left Right case
-    # root = tree.insert(root, 20, 0, 0)
-    # root = tree.insert(root, 30, 0, 0)
-    # root = tree.insert(root, 5, 0, 0)
-    # root = tree.insert(root, 6, 0, 0)
+            with open(sys.argv[3]) as inputFile:
+                for line in inputFile.readlines():
+                    line = line.split()
+                    resList = []
+                    tree.isPlayerBanned(line[0])
 
-    # Right Left case
-    # root = tree.insert(root, 20, 0, 0)
-    # root = tree.insert(root, 30, 0, 0)
-    # root = tree.insert(root, 40, 0, 0)
-    # root = tree.insert(root, 35, 0, 0)
-
-    # Left Left case
-    # root = tree.insert(root, 20, 0, 0)
-    # root = tree.insert(root, 30, 0, 0)
-    # root = tree.insert(root, 5, 0, 0)
-    # root = tree.insert(root, 4, 0, 0)
-
-    # Right Right case
-    # root = tree.insert(root, 20, 0, 0)
-    # root = tree.insert(root, 30, 0, 0)
-    # root = tree.insert(root, 40, 0, 0)
-    # root = tree.insert(root, 50, 0, 0)
+    time_taken_in_microseconds = (time.time_ns() - start_time) / 1000.0
+    print("total time in microseconds: " + str(time_taken_in_microseconds))
     # ==================================================
-
 
     # read input from file and insert into a tree ORG ON USER NAME
 
     # read names from stdin
     # print("Hi")
-
