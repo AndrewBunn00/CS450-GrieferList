08bc8e127be973636cebd9cea34822010e65b859
andrew.bunn <andrew.bunn@wsu.edu>
2022-10-20 17:40:12 -0700

    Updated scapegoat, continue fixing binary search (currently no parents stored)

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ class ScapeGoatTree:
         depth = 0
         # if tree empty, the new node will be the root
         if (self.root == None):
-            # root = ScapeGoatNode(user, serverBannedOn, timeOfBan)
             self.size += 1
             self.root = newNode
             return
@@ -67,25 +66,34 @@ class ScapeGoatTree:
         self.size += 1
 
         # if depth > alphaHeight(tree of size T) find scapegoat and rebalance
-        if (depth > self.alphaHeight()):
+        if (depth > self.alphaHeight() + 1):
             # To find goat, go backward up list of parents until we find highest imbalanced parent
             # set lowest parent as starting point
             highestParent = parentList[-1]
+            print("Finding Scapegoat...")
             for i, parent in enumerate(parentList[::-1]):
 
                 if (i > self.alphaHeight(parent)):
                     highestParent = parent
+                    break
 
             # Do in-order traversal of tree starting at the highest imbalanced parent to get arr of sorted vals
             subRoot = highestParent
             inOrderNodes = []
             inOrderNodes = self.inOrderTraversal(subRoot, inOrderNodes)
-            # TODO figure out why parent is not assigned on certain case... depth 13
+
             # use inOrderNodes to rebuild tree
-            newSubRoot = self.binarySearch(inOrderNodes, 0, len(inOrderNodes) - 1)
+            listOfNodesToRet = []
+            listForInsertion = self.binarySearch(inOrderNodes, 0, len(inOrderNodes) - 1, listOfNodesToRet)
+            newSubRoot = listForInsertion[0]
+
             if(highestParent != self.root):
                 newSubRoot.parent = highestParent.parent
-                highestParent = newSubRoot
+                if(highestParent.parent.left == highestParent):
+                    highestParent.parent.left = newSubRoot
+                else:
+                    highestParent.parent.right = newSubRoot
+                # highestParent = newSubRoot
             elif(highestParent == self.root):
                 self.root = newSubRoot
 
@@ -123,7 +131,7 @@ class ScapeGoatTree:
             self.inOrderTraversal(subRoot.right, inOrderNodes)
         return inOrderNodes
 
-    def binarySearch(self, inOrderNodes, low, high):
+    def binarySearch(self, inOrderNodes, low, high, listOfNodesToRet):
         if low > high:
             return
 
@@ -206,6 +214,7 @@ class AVLTree:
 
         return root
 
+
     def rotLeft(self, root, adjBalance):
         if not root.right:
             return root
@@ -219,21 +228,8 @@ class AVLTree:
             newRoot.balance = 0
             root.balance = 0
 
-        # newRoot = root.right
-        #
-        # root.right = root.right.left
-        # newRoot.left = root
-        # if (newRoot.balance == 0 and (root.left or root.right)):
-        #     newRoot.balance = -1
-        #     root.balance = 1
-        # elif (newRoot.balance == 0):
-        #     newRoot.balance = -1
-        #     root.balance = 0
-        # else:
-        #     newRoot.balance = 0
-        #     root.balance = 0
-
         return newRoot
+
 
     def rotRight(self, root, adjBalance):
         if not root.left:
@@ -247,36 +243,11 @@ class AVLTree:
         if(adjBalance):
             newRoot.balance = 0
             root.balance = 0
-            # if (newRoot.balance == 0 and (root.left or root.right)):
-            #     newRoot.balance = 1
-            #     root.balance = 1
-            # elif (newRoot.balance == 0):
-            #     newRoot.balance = 1
-            #     root.balance = 0
-            # else:
-            #     newRoot.balance = 0
-            #     root.balance = 0
-
-        # newRoot = root.left
-        #
-        # root.left = root.left.right
-        # newRoot.right = root
-        #
-        # if (newRoot.balance == 0 and (root.left or root.right)):
-        #     newRoot.balance = 1
-        #     root.balance = 1
-        # elif (newRoot.balance == 0):
-        #     newRoot.balance = 1
-        #     root.balance = 0
-        # else:
-        #     newRoot.balance = 0
-        #     root.balance = 0
 
         return newRoot
 
+
     def rotLeftRight(self, curNode):
-        # curNode.left = self.rotLeft(curNode.left)
-        # return self.rotRight(curNode, True)
         curNode.left = self.rotLeft(curNode.left, False)
         y = self.rotRight(curNode, False)
         x = y.right
@@ -297,8 +268,6 @@ class AVLTree:
 
 
     def rotRightLeft(self, curNode):
-        # curNode.right = self.rotRight(curNode.right, True)
-        # return self.rotLeft(curNode)
         curNode.right = self.rotRight(curNode.right, False)
         y = self.rotLeft(curNode, False)
         x = y.left
@@ -342,15 +311,71 @@ class AVLTree:
 
 
 
+def readFromStdIn(tree, root=None):
+    """
+    Reads input from standard in and puts it in lines
+    """
+    if(root == None):
+        for line in sys.stdin:
+            line = line.rstrip()
+            tree.isPlayerBanned(line)
+    else:
+        for line in sys.stdin:
+            line = line.rstrip()
+            tree.isPlayerBanned(root, line)
+
+
+
+# Remove \n from lines
+def stripEndlines(lines):
+    for i, line in enumerate(lines):
+        curLine = lines[i].rstrip()  # remove everything that is whitespace at the end of the line
+        lines[i] = curLine
+    return lines
+
+
 if __name__ == '__main__':
-    # SCAPEGOAT AND TEST CASES =========================================
 
-    # =========================================
+# AVL AND TEST CASES =========================================
+#     tree = AVLTree()
+#     root = None
 
-    # AVL AND TEST CASES =========================================
+    # for line in sys.stdin:
+    #     line = line.split()
+
+    # if(root == None):
+    #     root = AVLNode(10, 0, 0)
+
+    # Left Right case
+    # root = tree.insert(root, 20, 0, 0)
+    # root = tree.insert(root, 30, 0, 0)
+    # root = tree.insert(root, 5, 0, 0)
+    # root = tree.insert(root, 6, 0, 0)
+
+    # # Right Left case
+    # root = tree.insert(root, 20, 0, 0)
+    # root = tree.insert(root, 30, 0, 0)
+    # root = tree.insert(root, 40, 0, 0)
+    # root = tree.insert(root, 35, 0, 0)
+    #
+    # # Left Left case
+    # root = tree.insert(root, 20, 0, 0)
+    # root = tree.insert(root, 30, 0, 0)
+    # root = tree.insert(root, 5, 0, 0)
+    # root = tree.insert(root, 4, 0, 0)
+    #
+    # # Right Right case
+    # root = tree.insert(root, 20, 0, 0)
+    # root = tree.insert(root, 30, 0, 0)
+    # root = tree.insert(root, 40, 0, 0)
+    # root = tree.insert(root, 50, 0, 0)
+    #
+    # print("Did i survive")
+# ==================================================
+
     start_time = time.time_ns()
 
-    with open(sys.argv[2]) as file:
+    with open(sys.argv[2], 'r') as file:
         if (sys.argv[1] == "avl"):
 
             tree = AVLTree()
@@ -364,28 +389,22 @@ if __name__ == '__main__':
                 else:
                     root = tree.insert(root, line[0], line[1], line[2])
 
-            with open(sys.argv[3]) as inputFile:
-                for line in inputFile.readlines():
-                    line = line.split()
-                    resList = []
-                    tree.isPlayerBanned(root, line[0])
+            readFromStdIn(tree, root)
+
 
 
         elif(sys.argv[1] == "scapegoat"):
 
-            tree = ScapeGoatTree(0.9)
+            tree = ScapeGoatTree(0.75)
             root = None
 
             for line in file.readlines():
                 line = line.split()
                 tree.insert(line[0], line[1], line[2])
 
-            with open(sys.argv[3]) as inputFile:
-                for line in inputFile.readlines():
-                    line = line.split()
-                    resList = []
-                    tree.isPlayerBanned(line[0])
+            readFromStdIn(tree)
+
 
     time_taken_in_microseconds = (time.time_ns() - start_time) / 1000.0
     print("total time in microseconds: " + str(time_taken_in_microseconds))
-    # ==================================================
+
